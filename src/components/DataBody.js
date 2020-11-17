6b0f7f589e4dea34080f5fed9123f0e43d0e88a2
Andres Roldan <dresroldan@gmail.com>
2020-11-17 14:30:30 -0600

    added data body.js and data table.js

--- src/components/DataBody.js
+++ src/components/DataBody.js
@@ -1,19 +1,56 @@
 import React from "react";
 
-
 function DataBody({ users }) {
+  function formatDate(date) {
+    const dateArray = date.split("-");
+    const year = dateArray[0];
+    const month = dateArray[1];
+    const dayArray = dateArray[2].split("T");
+    const day = dayArray[0];
+    const formattedDate = [month, day, year].join("-");
+    return formattedDate;
+  }
 
+  return (
+    <tbody>
+      {users[0] !== undefined && users[0].name !== undefined ? (
+        // 6 attributes from user
+        users.map(({ login, name, picture, phone, email, dob }) => {
+          return (
+            <tr key={login.uuid}>
+              <td data-th="Image" className="align-middle">
+                <img
+                  src={picture.medium}
+                  alt={"profile image for" + name.first + " " + name.last}
+                  className="img-responsive"
+                />
+              </td>
 
+              <td data-th="Name" className="name-cell align-middle">
+                {name.first} {name.last}
+              </td>
 
-return(
-<div>
+              <td data-th="Phone" className="align-middle">
+                {phone}
+              </td>
 
+              <td data-th="Email" className="align-middle">
+                <a href={"mailto:" + email} target="__blank">
+                  {email}
+                </a>
+              </td>
 
-</div>
-
-
-)
-
+              <td data-th="DOB" className="align-middle">
+                {formatDate(dob.date)}
+              </td>
+            </tr>
+          );
+        })
+      ) : (
+        <> </>
+      )}
+    </tbody>
+  );
 }
 
 export default DataBody;
--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,26 +1,36 @@
 import React from "react";
 import DataBody from "./DataBody";
 
-
-
-
-function DataTable ({users}){
-return(
-
-
-
-<DataBody users={users}/>
-
-
-
-);
-
-
-
-
-
-
-
+function DataTable({ headings, users, handleSort }) {
+  return (
+    <div className="datatable mt-5">
+      <table
+        id="table"
+        className="table table-striped table-hover table-condensed"
+      >
+        <thead>
+          <tr>
+            {headings.map(({ name, width }) => {
+              return (
+                <th
+                  className="col"
+                  key={name}
+                  style={{ width }}
+                  onClick={() => {
+                    handleSort(name.toLowercase());
+                  }}
+                >
+                  {name}
+                  <span className="pointer"></span>
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <DataBody users={users}/>
+      </table>
+    </div>
+  );
 }
 
 export default DataTable;
